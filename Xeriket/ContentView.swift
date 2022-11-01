8c434c68d1277ed901c4e16497426f8bd0486064
Waris Works <warisruzi@gmail.com>
2022-11-01 15:35:54 +0900

    +animate

--- Xeriket/ContentView.swift
+++ Xeriket/ContentView.swift
@@ -8,10 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var showView: Bool = false
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
-            Home()
+            if showView {
+                Home()
+            }
         }
+        
         .frame(maxWidth: .infinity)
         .background{
             ZStack{
@@ -35,6 +39,11 @@ struct ContentView: View {
             .ignoresSafeArea()
         }
         .preferredColorScheme(.dark)
+        .onAppear{
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1){
+                showView = true
+            }
+        }
     }
 }
 
